e2f568ac6661470784d892249e6f44c2c68d1310
jwAndroid <cjd9408er@naver.com>
2022-09-08 01:42:32 +0900

    Prog search

--- src/navigation/screens/SearchScreen.tsx
+++ src/navigation/screens/SearchScreen.tsx
@@ -1,5 +1,11 @@
-import React, { memo, useRef, useState } from 'react';
-import { Pressable, Text, TextInput } from 'react-native';
+import React, { memo, useCallback, useRef, useState } from 'react';
+import {
+  FlatList,
+  ListRenderItem,
+  Pressable,
+  Text,
+  TextInput,
+} from 'react-native';
 import styled from '@emotion/native';
 import { useFocusEffect, useNavigation } from '@react-navigation/core';
 
@@ -28,18 +34,61 @@ const SearchBar = styled.TextInput(() => ({
   backgroundColor: 'skyblue',
 }));
 
+interface Data {
+  id: string;
+  text: string;
+  text2: string;
+}
+
+const data = [
+  { id: '1', text: '지웅이', text2: 'a' },
+  { id: '2', text: '지웅이123', text2: 'abc' },
+  { id: '3', text: '나리', text2: 'bgfd' },
+  { id: '4', text: '나리호리', text2: 'qwerr' },
+  { id: '5', text: '먹보나리', text2: 'ppp' },
+  { id: '6', text: '호랑이', text2: 'kk' },
+  { id: '7', text: '방탈출123', text2: 'ass' },
+  { id: '8', text: '123', text2: 'tyr' },
+  { id: '9', text: '대탈출', text2: 'a123' },
+];
+
 function SearchScreen() {
   const navigation = useNavigation<RootStackNavigationProp>();
 
   const ref = useRef<TextInput>(null);
 
   const [value, setValue] = useState('');
+  // const [listData, setListData] = useState<Data[]>(data);
 
   useFocusEffect(() => {
     if (ref) {
       ref.current?.focus();
     }
   });
+
+  const onPressItem = useCallback(
+    (item: Data) => () => {
+      console.log(item.text);
+    },
+    []
+  );
+
+  const renderItem = useCallback<ListRenderItem<Data>>(
+    ({ item }) => <Text onPress={onPressItem(item)}>{item.text}</Text>,
+    [onPressItem]
+  );
+
+  // const onChangeText = useCallback((text: string) => {
+  //   const prepared = listData.filter(
+  //     (data) => data.text.includes(text) || data.text2.includes(text)
+  //   );
+
+  //   console.log(prepared);
+
+  //   setValue(text);
+
+  //   setListData(prepared);
+  // }, []);
 
   return (
     <SafeAreaContainer>
@@ -52,7 +101,12 @@ function SearchScreen() {
           </Pressable>
         </SearchBarContainer>
 
-        <Text>data</Text>
+        <FlatList
+          style={{ margin: 10 }}
+          data={data}
+          keyExtractor={(item) => item.id}
+          renderItem={renderItem}
+        />
       </KeyboardContainer>
     </SafeAreaContainer>
   );
