899e0b74e2320672ad03914fdcb4b8b44fb2e08b
jwAndroid <cjd9408er@naver.com>
2022-09-20 02:19:39 +0900

    Update env

--- App.tsx
+++ App.tsx
@@ -5,6 +5,7 @@ import { Provider } from 'react-redux';
 import { store } from './src/redux/store';
 import Main from './Main';
 
+// update env
 function App() {
   return (
     <Provider store={store}>
