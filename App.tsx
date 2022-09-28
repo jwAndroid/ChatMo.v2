34cc9a61b3cec3eeecc573930fa37a7633059481
jwAndroid <cjd9408er@naver.com>
2022-09-28 11:01:56 +0900

    Update app

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import React, { memo } from 'react';
 import { Provider } from 'react-redux';
-
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+
 import { store } from './src/redux/store';
 import Main from './Main';
 
